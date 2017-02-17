e62f5ed55fb107a6a7ac9df86fa07b09b7ba9c9e
Ivan Rimbaud <ivan.rimbaud@gmail.com>
2017-02-17 11:34:26 -0200

    add figaro

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,33 @@
 class DosesController < ApplicationController
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredients = Ingredient.all
+    @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredients = Ingredient.all
+    @dose = Dose.new(params_dose)
+    @dose.cocktail = @cocktail
+    if @dose.save
+    redirect_to cocktails_path
+    else
+    render :new
+    end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
   end
+
+
+  private
+
+  def params_dose
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
 end
